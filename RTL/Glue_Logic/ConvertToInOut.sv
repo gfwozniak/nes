432177bf9a4b63c9122b70da82a66a5b6132f62e
Gabe Wozniak <gfw3@illinois.edu>
2023-11-28 14:15:50 -0600

    Add glue dir

---
+++ RTL/Glue_Logic/ConvertToInOut.sv
@@ -0,0 +1,20 @@
+module ConvertToInOut #(parameter WIDTH=8)(
+	input logic [WIDTH-1:0] indata,
+	output logic [WIDTH-1:0] outdata,
+	input logic rw,
+	inout logic [WIDTH-1:0] inoutdata
+	);
+	
+	parameter  WRITE = 1,
+	           READ = 0;
+	
+	assign inoutdata = (rw == WRITE) ? indata : 8'hzz;
+
+
+
+always_comb begin
+    if (rw == READ)
+        outdata = inoutdata;
+end
+	
+endmodule
